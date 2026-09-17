dde3e8992c10f696a0df675deedb8e3b76c5b2fb
Zaksonboy <135555776+Zaksonboy@users.noreply.github.com>
2026-09-17 20:29:48 +0100

    Fix function name casing for closeFeaturePage

--- script.js
+++ script.js
@@ -590,7 +590,7 @@ function goToPage(tab) {
   if (tab === 'contacts') renderContacts();
 }
 
-function closeFeaturepage() {
+function closeFeaturePage() {
   document.getElementById('lookupPanel').classList.remove('show');
   document.getElementById('contactsPanel').classList.remove('show');
 }
